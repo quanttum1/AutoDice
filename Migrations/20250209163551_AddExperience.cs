302322181472fc27f787ac9838cdb0d1d7f0170a
Taras Tuz <zutsarat@protonmail.com>
2025-02-09 16:38:26 +0000

    Add Experience and ExperienceCanBeSpent to the Models.Character

---
+++ Migrations/20250209163551_AddExperience.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoDice.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExperience : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Experience",
+                table: "Characters",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "ExperienceCanBeSpent",
+                table: "Characters",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Experience",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "ExperienceCanBeSpent",
+                table: "Characters");
+        }
+    }
+}
--- Models/Character.cs
+++ Models/Character.cs
@@ -10,6 +10,9 @@ public class Character
 
     public string? ImagePath { get; set; }
 
+    public double Experience { get; set; } = 0.0;
+    public double ExperienceCanBeSpent { get; set; } = 0.0;
+
     public ICollection<CharacterSkill> CharacterSkills { get; set; } = new List<CharacterSkill>();
     public ICollection<CharacterHealth> CharacterHealths { get; set; } = new List<CharacterHealth>();
     public ICollection<CharacterInventory> CharacterInventories { get; set; } = new List<CharacterInventory>();
